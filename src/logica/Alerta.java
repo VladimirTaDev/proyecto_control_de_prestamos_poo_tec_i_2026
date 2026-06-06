c8374c61afcdd2e7e08fd6274d889e0544af8fb9
Vladimir Tarassenko <99820518+VladimirTaDev@users.noreply.github.com>
2026-06-06 12:12:23 -0600

    Agregar campos y métodos de acceso a Alerta y Persona
    Introducir constructores, campos y métodos de acceso básicos para Alerta y Persona.
    
    Clase Alerta:
    * Agregar campos: mensaje, fechaAlerta (LocalDateTime), esRecurrente e intervalo.
    * Incluir constructor y métodos getters/setters.
    
    Clase Persona:
    * Agregar campos: nombre, telefono, email y una lista de prestamos (inicializada como ArrayList).
    * Incluir constructor, getters/setters y métodos para obtener/añadir préstamos.
    * Agregar tareas pendientes (TODOs) para la validación de teléfono y correo electrónico.
    
    Se añadieron las importaciones necesarias para java.time.LocalDateTime y java.util.List.

--- src/logica/Alerta.java
+++ src/logica/Alerta.java
@@ -1,9 +1,33 @@
 package logica;
 
+import java.time.LocalDateTime;
+
 public class Alerta {
+	private String mensae;
+	private LocalDateTime fechaAlerta;
+	private boolean esRecurrente;
+	private int intervalo;
 
-	public Alerta() {
-		// TODO Auto-generated constructor stub
+	public Alerta(String mensae, LocalDateTime fechaAlerta, boolean esRecurrente, int intervalo) {
+		this.mensae = mensae;
+		this.fechaAlerta = fechaAlerta;
+		this.esRecurrente = esRecurrente;
+		this.intervalo = intervalo;
 	}
-
+	
+	public void setMensae(String mensae) {
+		this.mensae = mensae;
+	}
+	
+	public String getMensae() {
+		return mensae;
+	}
+	
+	public LocalDateTime getFechaAlerta() {
+		return fechaAlerta;
+	}
+	
+	public void setFechaAlerta(LocalDateTime fechaAlerta) {
+		this.fechaAlerta = fechaAlerta;
+	}
 }
--- src/logica/Persona.java
+++ src/logica/Persona.java
@@ -1,9 +1,53 @@
 package logica;
 
+import java.util.List;
+
 public class Persona {
+	private String nombre;
+	private String telefono;
+	private String email;
+	private List<Prestamo> prestamos;
 
-	public Persona() {
-		// TODO Auto-generated constructor stub
+	public Persona(String nombre, String telefono, String email) {
+		this.nombre = nombre;
+		this.telefono = telefono;
+		this.email = email;
+		this.prestamos = new java.util.ArrayList<>();
 	}
-
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getTelefono() {
+		return telefono;
+	}
+	
+	public void setTelefono(String telefono) {
+		// TODO: Validar formato de teléfono
+		this.telefono = telefono;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		// TODO: Validar formato de email
+		this.email = email;
+	}
+	
+	public List<Prestamo> getPrestamo() {
+		// No implementado
+		return prestamos;
+	}
+	
+	public void setPrestamo(Prestamo prestamo) {
+		prestamos.add(prestamo);
+	}
+	
 }
